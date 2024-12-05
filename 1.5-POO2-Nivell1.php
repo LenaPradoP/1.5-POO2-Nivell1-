8d905f0a221e22a50044a98a2664c8dbc319cab8
LenaPradoP <lena@atajo.es>
2024-12-05 11:48:08 +0100

    Cambio br por .PHP_EOL

--- 1.5-POO2-Nivell1.php
+++ 1.5-POO2-Nivell1.php
@@ -3,10 +3,10 @@ require("ClassAnimal.php");
 require("ClassCat.php");
 require("ClassDog.php");
 
-echo "<h1><u>Ejercicio 1.5 - POO2 </h1></u>";
+echo "Ejercicio 1.5 - POO2" .PHP_EOL;
 
 $cat1 = new Cat('Olivia');
-echo "El gato suena: " . $cat1->makeSound() . "<br>";
+echo "El gato suena: " . $cat1->makeSound() .PHP_EOL;
 $dog1 = new Dog('Polka');
 echo 'El perro suena: ' . $dog1->makeSound();
 
